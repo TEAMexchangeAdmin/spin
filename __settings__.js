a26bc6f0da72af5702c38e3af29254dbe625dd2a
TEAMexchangeAdmin <62411985+TEAMexchangeAdmin@users.noreply.github.com>
2022-10-28 14:18:04 +0800

    update

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1572487.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 108036332, 108036317, 108036328, 108036323, 108036330, 108226528 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
